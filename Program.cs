61c8005b19a0d8a63c3383e86f63685d6baed25a
Abyan Falah <abyanf410@gmail.com>
2022-11-06 23:14:56 +0700

    initial commit

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿internal class Program
+{
+	private static void Main(string[] args)
+	{
+		Console.Clear();
+
+		Console.Write("What is your age right now? ");
+
+		int age = Convert.ToInt16(Console.ReadLine());
+		Console.WriteLine("Your age is: " + age);
+
+		Console.ReadLine();
+		Console.Clear();
+	}
+}
